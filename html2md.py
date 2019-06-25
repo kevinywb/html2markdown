9057fa2603ab6ec5d6745727c69ab74974b4fcc5
kevinywb <kevinywb@vip.qq.com>
2019-06-25 15:44:24 +0800

    update output property

--- html2md.py
+++ html2md.py
@@ -3,29 +3,33 @@ from html.parser import HTMLParser
 
 class Html2Markdown(HTMLParser):
     # output markdown text
-    output = ''
+    __output = ''
 
     # take a unique placeholder then replace text
-    _placeholder = '3f4ec2b893ce4f1ab0f4c0861ef9dae7'
+    __placeholder = '3f4ec2b893ce4f1ab0f4c0861ef9dae7'
 
     # mark content between start tag and end tag
-    _content = ''
+    __content = ''
 
     # mark hidden between start tag and end tag
-    _hidden = False
+    __hidden = False
 
     # mark new line between start tag and end tag
-    _newline = True
+    __newline = True
 
     # mark prefix line between start tag and end tag
-    _prefix = False
+    __prefix = False
+
+    # set no space mode
+    __no_space = True
 
     # define replacemen rules - 0: starttag, 1: endtag
-    _rule_replacement = {
+    __rule_replacement = {
         'a': ('', ''),
         'blockquote': ('\n', '\n'),
         'code': (' ``` ', ' ``` '),
-        'em': ('*', '*'),
+        'div': ('', '\n'),
+        # 'em': (' *', '* '),
         'h1': ('# ', '\n'),
         'h2': ('## ', '\n'),
         'h3': ('### ', '\n'),
@@ -33,12 +37,21 @@ class Html2Markdown(HTMLParser):
         'h5': ('##### ', '\n'),
         'h6': ('###### ', '\n'),
         'hr': ('', ' ----- \n'),
-        'img': ('', '\n'),
+        'img': ('', '\n\n'),
         'p': ('', '\n'),
         'pre': ('', '\n'),
-        'strong': ('**', '**'),
+        'strong': (' **', '** '),
         'ul': ('\n', '\n')
     }
+
+    @property
+    def output(self):
+        r = self.__output
+        r = r.replace('  ', '')
+        r = r.replace('\n\n\n', '\n')
+        r = r.replace('****', '')
+        r = r.replace('```  ```', '')
+        return r.strip()
 
     # default parse
     def default_parse(self, tag, alone):
@@ -46,65 +59,70 @@ class Html2Markdown(HTMLParser):
             i = 0
         else:
             i = 1
-        if tag in self._rule_replacement:
-            it = self._rule_replacement[tag]
-            self.output += it[i]
+        if tag in self.__rule_replacement:
+            it = self.__rule_replacement[tag]
+            self.__output += it[i]
 
     # handle start tag
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             for (key, val) in attrs:
                 if key == 'href':
-                    self._content = f'[{self._placeholder}]({val})'
+                    self.__content = f'[{self.__placeholder}]({val})'
 
         if tag == 'blockquote':
-            self._prefix = True
+            self.__prefix = True
 
         if tag == 'code':
-            self._content = self._placeholder
+            self.__content = self.__placeholder
+            self.__no_space = False
 
         if tag == 'figcaption':
-            self._hidden = True
+            self.__hidden = True
 
         if tag == 'li':
-            if self._prefix is True:
-                self.output += '>'
+            if self.__prefix is True:
+                self.__output += '>'
             else:
-                self.output += '* '
-            self._newline = False
+                self.__output += '* '
+            self.__newline = False
 
         if tag == 'p':
-            if self._newline is True:
-                self.output += '\n'
+            if self.__newline is True:
+                self.__output += '\n'
 
         if tag == 'pre':
-            self._rule_replacement['code'] = (' ``` \n', ' ``` ')
+            self.__rule_replacement['code'] = (' ``` \n', ' \n ``` ')
 
         # default parse
         self.default_parse(tag, True)
 
     # handle text data
     def handle_data(self, data):
-        if len(self._content) > 0:
-            self.output += self._content.replace(self._placeholder, data)
-            self._content = ''
-        elif self._hidden is False:
-            self.output += data
+        data = data.replace('\xa0', '')
+        if len(self.__content) > 0:
+            self.__output += self.__content.replace(self.__placeholder, data)
+            self.__content = ''
+        elif self.__hidden is False:
+            self.__output += data
 
     # handle end tag
     def handle_endtag(self, tag):
         if tag == 'blockquote':
-            self.output += '\n'
-            self._prefix = False
+            self.__output += '\n'
+            self.__prefix = False
+
+        if tag == 'code':
+            self.__no_space = True
 
         if tag == 'figcaption':
-            self._hidden = False
+            self.__hidden = False
 
         if tag == 'li':
-            self._newline = True
+            self.__newline = True
 
         if tag == 'pre':
-            self._rule_replacement['code'] = (' ``` ', ' ``` ')
+            self.__rule_replacement['code'] = (' ``` ', ' ``` ')
 
         # default parse
         self.default_parse(tag, False)
@@ -112,13 +130,13 @@ class Html2Markdown(HTMLParser):
     # handle never end tag
     def handle_startendtag(self, tag, attrs):
         if tag == 'br':
-            if self._newline is True:
-                self.output += '\n'
+            if self.__newline is True:
+                self.__output += '\n'
 
         if tag == 'img':
             for (key, val) in attrs:
                 if key == 'data-src':
-                    self.output += f'![]({val}&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1)'
+                    self.__output += f'![]({val}&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1)'
 
         # default parse
-        self.default_parse(tag, True)
+        self.default_parse(tag, False)
